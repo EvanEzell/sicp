ff43feafa9f2494535525628eab0b66eb8716e64
EvanEzell <evanezell595@gmail.com>
2020-10-15 16:50:36 -0400

    removed unused procedures, added comments

--- solutions/4.6.scm
+++ solutions/4.6.scm
@@ -1,15 +1,13 @@
 ; Exercise 4.6
 ; Author: Evan Ezell
 
+; syntax procedures
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 
 (define (let-arg-list exp) (cdr exp))
 (define (let-par item) (car item))
 (define (let-arg item) (cdr item))
-
-(define (first-term term-list) (car term-list))
-(define (rest-terms term-list) (cdr term-list))
 
 (define (extract-let-body exp) (cddr exp))
 
@@ -19,12 +17,13 @@
 (define (extract-let-args exp)
   (map let-arg (let-arg-list exp)))
 
+; transformation procedure or macro
 (define (let->combination exp)
   (cons (make-lambda (extract-let-pars exp)
                      (extract-let-body exp))
         (extract-let-args exp)))
 
-; assuming data-directed eval
+; install new transformation into data-directed eval
 (put 'eval 'let
      (lambda (exp env)
        (let->combination exp)))
