f03a888e1108c7e6b40ae352691e41d85ba9acf3
EvanEzell <evanezell595@gmail.com>
2020-10-28 22:57:46 -0400

    init

---
+++ solutions/3.7.scm
@@ -0,0 +1,27 @@
+;;; Exercise 3.7
+;;; Author: Evan Ezell
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      (error "Insufficient funds")))
+  (define (deposit amount)
+    (begin (set! balance (+ balance amount))
+           balance))
+  (define (dispatch psswd m)
+    (cond ((not (eq? password psswd))
+           (lambda (amount) (error "Incorrect password")))
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+    dispatch)
+
+(define (make-joint account password new-password)
+  (define (dispatch psswd m)
+    (if (not (eq? new-password psswd))
+      (lambda (amount) (error "Incorrect password"))
+      (account password m)))
+  dispatch)
