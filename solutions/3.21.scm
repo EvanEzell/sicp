da052d21cfee6f1aeb15f7c374a3a29392594126
EvanEzell <evanezell595@gmail.com>
2021-03-30 11:12:04 -0400

    fixed indentation

--- solutions/3.21.scm
+++ solutions/3.21.scm
@@ -7,9 +7,9 @@
 (define (front-ptr queue) (car queue))
 (define (rear-ptr queue) (cdr queue))
 (define (set-front-ptr! queue item)
-(set-car! queue item))
+  (set-car! queue item))
 (define (set-rear-ptr! queue item)
-(set-cdr! queue item))
+  (set-cdr! queue item))
 
 (define (empty-queue? queue)
   (null? (front-ptr queue)))
